18efc689cbaafbb0c1609bd8e440a192802d09e0
ATIK-Tech <mail@atiktech.pro>
2017-11-09 15:15:03 -0600

    Update 0.0.1
    Includes:
    - Added UI

---
+++ Project BI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project BI
+//
+//  Created by ATIK on 11/8/17.
+//  Copyright © 2017 ATIK_Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
